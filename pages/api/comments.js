52c7e93f78b3e822fd082969e232405ee2d0cb3e
Arachchige P.D.A.M <it20121556@my.sliit.lk>
2021-12-22 18:58:33 +0530

    first commit

---
+++ pages/api/comments.js
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { GraphQLClient, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+// const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function asynchandler(req, res) {
+	const graphQlClient = new GraphQLClient(graphqlAPI, {
+		headers: {
+			authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+		},
+	});
+
+	const query = gql`
+		mutation CreateComment(
+			$name: String!
+			$email: String!
+			$comment: String!
+			$slug: String!
+		) {
+			createComment(
+				data: {
+					name: $name
+					email: $email
+					comment: $comment
+					post: { connect: { slug: $slug } }
+				}
+			) {
+				id
+			}
+		}
+	`;
+
+	try {
+		const result = await graphQlClient.request(query, req.body);
+
+		return res.status(200).send(result);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send(error);
+	}
+}
